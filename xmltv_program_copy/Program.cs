d3324effa74bf4d916ce56196c334a561585c412
Harry G <harrygg@gmail.com>
2017-01-17 15:54:22 +0200

    Fixed config loading and

--- xmltv_program_copy/Program.cs
+++ xmltv_program_copy/Program.cs
@@ -6,8 +6,6 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace xmltv_program_copy
@@ -17,21 +15,23 @@ namespace xmltv_program_copy
     static String inputXml = "epg.xml";
     static String outputXml = "epg_copy.xml";
     static String configXml = "";
+    static XDocument inputEpg = null;
+    static Epg outputEpg = new Epg();
+    static Config config = null;
     static bool unifyIds = true;
 
     static void Main(string[] args)
     {
+      inputXml = (args.Length > 0) ? args[0] : inputXml;
+      outputXml = (args.Length > 1) ? args[1] : inputXml;
+      configXml = (args.Length > 2) ? args[2] : configXml;
+
       Console.WriteLine("#####################################################");
       Console.WriteLine("#         xmltv_program_copy by Harry_GG            #");
-      Console.WriteLine("#---------------------------------------------------#");
-
-      inputXml = (args.Length > 0) ? args[0] : inputXml;
+      Console.WriteLine("#---------------------------------------------------#");   
       Console.WriteLine("Input EPG file: {0}", inputXml);
-
-      outputXml = (args.Length > 1) ? args[1] : inputXml;
       Console.WriteLine("Output EPG file: {0}", outputXml);
 
-      configXml = (args.Length > 2) ? args[2] : configXml;
       if (configXml != "")
       {
         Console.WriteLine("Using config file: {0}", configXml);
@@ -42,173 +42,162 @@ namespace xmltv_program_copy
         return;
       }
 
-      XDocument xmlContent;
-      try
-      {
-        xmlContent = XDocument.Load(configXml);
-      }
-      catch (Exception ex)
-      {
-        Console.WriteLine("Unable to read config file");
-        Console.WriteLine(ex.ToString());
+      config = new Config(configXml);
+      if (config.channels == null)
         return;
-      }
-
-      ///Get list of channels that will be copied
-      var channelsToCopy = (from c in xmlContent.Element("root").Element("channels").Elements("channel") select c).ToList();
-
-      ///Get the elements that will be removed from channel and programme tags
-      var el = xmlContent.Element("root").Element("elements").Element("remove");
-      var globalElementsToRemove = el != null ? el.Value.Split(',').ToList() : new List<string>();
-
-      ///Get the elements that will be removed from channel and programme tags
-      var globalElementsToKeep = new List<string>() { "display-name", "title" }; //Unremovable elements
-      el = xmlContent.Element("root").Element("elements").Element("keep");
-      if (el != null)
-        globalElementsToKeep.AddRange(el.Value.Split(',').ToList());
-
-      ///Get the elements that will be added to the channel tag
-      el = xmlContent.Element("root").Element("elements").Element("add");
-      var globalElementsToAdd = new List<XElement>();
-      if (el != null)
-        globalElementsToAdd = el.Descendants().ToList();
 
       ///Read the input EPG
+      inputEpg = GetInputDocument(inputXml);
+      if (inputEpg == null)
+        return;
+
+      //Output document
+      config.channels.ForEach(channel => CopyChannel(channel));
+      outputEpg.Save(outputXml);
+    }
+
+    static void CopyChannel(XElement channelToCopy)
+    {
+      var name = channelToCopy.Attribute("name").Value;
+      var attr = channelToCopy.Attribute("display-name");
+      var newName = (attr != null && attr.Value != "") ? attr.Value : name;
+
+      //Get local modification rules and add global ones 
+      var elementsToKeep = GetLocalRules(channelToCopy, "keep");
+      elementsToKeep.AddRange(config.keepElements);
+
+      var elementsToRemove = GetLocalRules(channelToCopy, "remove");
+      elementsToRemove.AddRange(config.removeElements);
+
+      var temp = channelToCopy.Element("add");
+      var elementsToAdd = temp != null ? temp.Descendants().ToList() : new List<XElement>();
+      elementsToAdd.AddRange(config.addElements);
+
+      var channel = GetChannel(name);
+      if (channel != null)
+      {
+        var programmes = GetProgrammes(channel.Attribute("id").Value);
+        if (programmes != null)
+        {
+          ///Modify display-name if new value is provided
+          var channelName = channel.Element("display-name").Value;
+          if (channelName != newName)
+          {
+            channel.Element("display-name").Value = newName;
+            Console.WriteLine("Modifying channel name. Replaced \"{0}\" with \"{1}\"", channelName, newName);
+            channelName = newName;
+          }
+
+          ///Unify display-name and channel id
+          if (unifyIds)
+          {
+            if (channel.Attribute("id").Value != channelName)
+              channel.Attribute("id").Value = channelName;
+            programmes.ForEach(p => p.Attribute("channel").Value = channelName);
+          }
+
+          ///Remove elements from channel node
+          channel.Descendants()
+            .Where(c => 
+              !elementsToKeep.Contains(c.Name.LocalName) && 
+              (elementsToRemove.Contains("all") || elementsToRemove.Contains(c.Name.LocalName))
+              ).Remove();
+
+          ///Remove elements from programmes node
+          programmes.ForEach(p => RemoveElements(ref p, elementsToRemove, elementsToKeep));
+
+          ///Add elements to channel tag
+          elementsToAdd.ForEach(c => channel.Add(c));
+          elementsToRemove.Clear();
+          elementsToKeep.Clear();
+          elementsToAdd.Clear();
+
+          Console.WriteLine("Adding channel {0} with {1} programmes", name, programmes.Count);
+          outputEpg.channels.Add(channel);
+          outputEpg.programmes.AddRange(programmes);
+        }
+      }
+      else
+        Console.WriteLine("Channel {0} was not found in guide", name);
+    }
+
+    static XElement GetChannel(String name)
+    {
       try
       {
-          xmlContent = GetInputDocument(inputXml);
+        return (from c in inputEpg.Elements("tv").Elements("channel")
+                   where c.Element("display-name").Value == name select c).First();
+      }
+      catch
+      {
+        Console.WriteLine("Channel with name \"{0}\" was not found in the guide", name);
+        return null;
+      }
+    }
+
+    static List<XElement> GetProgrammes(String channelId)
+    {
+      try
+      {
+        return (from c in inputEpg.Elements("tv").Elements("programme")
+                where c.Attribute("channel").Value == channelId
+                select c).ToList();
       }
       catch (Exception ex)
       {
-        Console.WriteLine("Unable to read guide");
+        Console.WriteLine("Error getting programme for channel {0}", channelId);
         Console.WriteLine(ex.ToString());
-        return;
+        return null;
       }
+    }
 
-      //Output document
-      var tv = new XElement("tv");
-      var eChannels = new List<XElement>();
-      var eProgrammes = new List<XElement>();
-      var epg = new XDocument(new XDeclaration("1.0", "utf-8", null), tv);
+    static List<String> GetLocalRules(XElement channelToCopy, String elementName)
+    {
+      XElement temp = channelToCopy.Element(elementName);
+      var elements = temp != null ? temp.Value.Split(',').ToList() : new List<String>();
+      return elements;
+    }
 
-      foreach (var channelToCopy in channelsToCopy)
+    static XDocument GetInputDocument(string file)
+    {
+      try
       {
-        var name = channelToCopy.Attribute("name").Value;
-        var attr = channelToCopy.Attribute("display-name");
-        var newName = (attr != null && attr.Value != "") ? attr.Value : name; 
-
-        //Add local rules
-        el = channelToCopy.Element("keep");
-        var localElementsToKeep = el != null ? el.Value.Split(',').ToList() : new List<String>();
-        localElementsToKeep.AddRange(globalElementsToKeep);
-
-        el = channelToCopy.Element("remove");
-        var localElementsToRemove = el != null? el.Value.Split(',').ToList() : new List<String>();
-        localElementsToRemove.AddRange(globalElementsToRemove);
-        var removeAll = localElementsToRemove.Contains("all");
-
-        el = channelToCopy.Element("add");
-        var localElementsToAdd = el != null ? el.Descendants().ToList() : new List<XElement>();
-        localElementsToAdd.AddRange(globalElementsToAdd);
-
-        XElement channel = null; 
-        try
+        if (file.StartsWith("http") || file.StartsWith("ftp"))
         {
-          channel = (from c in xmlContent.Elements("tv").Elements("channel")
-                         where c.Element("display-name").Value == name
-                         select c).First();
-        } catch {
-          Console.WriteLine("Channel with name \"{0}\" was not found in the guide", name);
-        }
-
-        if (channel != null)
-        { 
-          var programmes = (from c in xmlContent.Elements("tv").Elements("programme")
-            where c.Attribute("channel").Value == channel.Attribute("id").Value select c).ToList();
-
-          if (programmes.Count() > 0)
-          {
-            ///Modify display-name if new value is provided
-            var channelName = channel.Element("display-name").Value;
-            if (channelName != newName)
-            {
-              channel.Element("display-name").Value = newName;
-              Console.WriteLine("Modifying channel name. Replaced \"{0}\" with \"{1}\"", channelName, newName);
-              channelName = newName;
-            }
-
-            ///Unify display-name and channel id
-            if (unifyIds)
-            {
-              if (channel.Attribute("id").Value != channelName)
-                channel.Attribute("id").Value = channelName;
-              programmes.ForEach(p => p.Attribute("channel").Value = channelName);
-            }
-
-            ///Remove elements from channel node
-            channel.Descendants().Where(c => (removeAll || localElementsToRemove.Contains(c.Name.LocalName))
-                && !localElementsToKeep.Contains(c.Name.LocalName)).Remove();
-
-            ///Remove elements from programmes node
-            programmes.ForEach(p => RemoveElements(ref p, localElementsToRemove, localElementsToKeep));
-
-            ///Add elements to channel tag
-            localElementsToAdd.ForEach(c => channel.Add(c));
-
-            localElementsToRemove.Clear();
-            localElementsToKeep.Clear();
-            localElementsToAdd.Clear();
-
-            Console.WriteLine("Adding channel {0} with {1} programmes", name, programmes.Count);
-            eChannels.Add(channel);
-            eProgrammes.AddRange(programmes);
-          }
+          Console.WriteLine("Starting download...");
+          var name = DownloadFile(file);
+          if (name == null)
+            Console.WriteLine("Unable to download file {0}. Please download manually!", name);
           else
           {
-            Console.WriteLine("Channel {0} has no programmes and will be skipped", name);
+            Console.WriteLine("\nFile downloaded! Saved to {0}", name);
+            file = name;
           }
         }
-        else
-          Console.WriteLine("Channel {0} was not found in guide", name);
-      }
 
-      tv.Add(eChannels.ToArray());
-      tv.Add(eProgrammes.ToArray());
-      epg.Save(outputXml);
+        if (file.EndsWith("gz"))
+        {
+          Console.WriteLine("Starting decompress...");
+          var name = Decompress(file);
+          if (name == null)
+            Console.WriteLine("Unable to decompress {0}. Please unzip manually.", file);
+          else
+          {
+            file = name;
+            Console.WriteLine("File decompressed to {0}", name);
+          }
+        }
+        return XDocument.Load(file);
+
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.ToString());
+        return null;
+      }
     }
 
-    private static XDocument GetInputDocument(string file)
-    {
-      if (file.StartsWith("http") || file.StartsWith("ftp"))
-      {
-        Console.WriteLine("Starting download...");
-        var name = DownloadFile(file);
-        if (name == null)
-          Console.WriteLine("Unable to download file {0}. Please download manually!", name);
-        else
-        {
-          Console.WriteLine("\nFile downloaded! Saved to {0}", name);
-          file = name;
-        }
-      }
-
-
-      if (file.EndsWith("gz"))
-      {
-        Console.WriteLine("Starting decompress...");
-        var name = Decompress(file);
-        if (name == null)
-          Console.WriteLine("Unable to decompress {0}. Please unzip manually.", file);
-        else
-        {
-          file = name;
-          Console.WriteLine("File decompressed to {0}", name);
-        }
-      }
-      return XDocument.Load(file);
-    }
-
-    private static String DownloadFile(string file)
+    static String DownloadFile(string file)
     {
       try
       { 
